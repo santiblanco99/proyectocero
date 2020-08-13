7697944eaae466769685c8016afbd698a09c782b
Santiago <santiagowhite99@hotmail.com>
2020-08-13 18:21:25 -0500

    Cambios en push

--- db/index.js
+++ db/index.js
@@ -23,15 +23,15 @@ const getUserById = (request, response) => {
 };
 
 const createUser = (request, response) => {
-  const { name, email } = request.body
+  const { name, email, lastname, password } = request.body
   console.log(request.body);
 
-  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, result) => {
+  pool.query('INSERT INTO users (name, lastname, email, password) VALUES ($1, $2, $3, $4)', [name, lastname, email, password], (error, result) => {
     if (error) {
       throw error
     }
     console.log(result);
-    response.status(201).send(`User added with ID: ${result.insertId}`)
+    response.status(201).send(`User added`)
   })
 }
 
