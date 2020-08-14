2ddda1b37597f5dc525ceba99a90ec9ee4934215
Santiago <santiagowhite99@hotmail.com>
2020-08-14 12:21:14 -0500

    error

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -4,18 +4,18 @@ var userDB = require('../db/userDB');
 var verifyToken = require('../auth/verifyToken');
 var router = express.Router();
 
-router.get('/events',verifyToken, async(req,res) => {
+router.get('/events', verifyToken, async (req, res) => {
     try {
         var loggedUser = await userDB.getUserById(req.userId);
         console.log(loggedUser);
         var events = await eventsDB.getUserEvents(loggedUser.email);
         res.json(events);
     } catch (error) {
-        
+
     }
 });
 
-router.get('/events/:id',verifyToken, async (req,res) => {
+router.get('/events/:id', verifyToken, async (req, res) => {
     var id = req.params.id;
     try {
         var event = await eventsDB.getEventById(id);
@@ -27,17 +27,17 @@ router.get('/events/:id',verifyToken, async (req,res) => {
 });
 
 
-router.post('/events',verifyToken,async(req,res)=> {
+router.post('/events', verifyToken, async (req, res) => {
     try {
         const loggedUser = await userDB.getUserById(req.userId);
-        (event_name, event_category, event_place, event_address, event_initial_date,
-            event_final_date,event_type) = req.body;
-        
-        await eventsDB.createEvent(event_name,event_category,event_place,event_address,event_initial_date,event_final_date,
-            event_type,loggedUser.email);
+        const { event_name, event_category, event_place, event_address,
+            event_initial_date, event_final_date, event_type } = req.body;
+
+        await eventsDB.createEvent(event_name, event_category, event_place, event_address, event_initial_date, event_final_date,
+            event_type, loggedUser.email);
         res.redirect('/events');
     } catch (error) {
-        
+
     }
 });
 
