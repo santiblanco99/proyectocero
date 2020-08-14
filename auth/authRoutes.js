58a7a7eb83ef884b400491150650c16ae2ec0c82
Santiago <santiagowhite99@hotmail.com>
2020-08-13 21:27:09 -0500

    error

--- auth/authRoutes.js
+++ auth/authRoutes.js
@@ -30,14 +30,14 @@ router.post('/register', (req, res) => {
 
 });
 
-router.get('/me', function (req, res) {
+router.get('/me', async (req, res) => {
     var token = req.headers['x-access-token'];
     if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
 
     jwt.verify(token, process.env['secret'], function (err, decoded) {
         if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
 
-        var user = userDB.getUserById(decoded.id);
+        var user = await userDB.getUserById(decoded.id);
         console.log(user);
         res.status(200).json(user);
     });
