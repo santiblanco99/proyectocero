fd20d50a2c26f323b3f6dc6eeff2315e957f0358
Santiago <santiagowhite99@hotmail.com>
2020-08-13 21:30:23 -0500

    error

--- db/userDB.js
+++ db/userDB.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool
 const pool = new Pool();
 
 const getUserById = async (id) => {
-    var res = await  pool.query('SELECT * FROM USERS WHERE email = $1',[id]);
+    var res = await  pool.query('SELECT name,lastname,email FROM USERS WHERE email = $1',[id]);
     console.log(res.rows[0]);
     return res.rows[0];
   };
