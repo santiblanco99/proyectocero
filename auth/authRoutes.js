fc80b7719d8c56a8d81112752468cadcf9536c18
Santiago <santiagowhite99@hotmail.com>
2020-08-13 22:02:01 -0500

    error

--- auth/authRoutes.js
+++ auth/authRoutes.js
@@ -1,6 +1,3 @@
-const Pool = require('pg').Pool
-const pool = new Pool();
-
 const userDB = require('../db/userDB');
 
 var express = require('express');
@@ -12,25 +9,24 @@ router.use(bodyParser.json());
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 
-router.post('/register', (req, res) => {
-    const { name, email, lastname, password } = req.body
-    console.log(req.body);
-    var hashedPassword = bcrypt.hashSync(password);
-
-    pool.query('INSERT INTO users (name, lastname, email, password) VALUES ($1, $2, $3, $4)', [name, lastname, email, hashedPassword], (error, result) => {
-        if (error) {
-            throw error
-        }
-        console.log(result);
+router.post('/register', async (req, res) => {
+    try {
+        const { name, email, lastname, password } = req.body
+        console.log(req.body);
+        var hashedPassword = bcrypt.hashSync(password);
+        await userDB.createUser(name, lastname, email, hashedPassword);
         var token = jwt.sign({ id: email }, process.env['secret'], {
             expiresIn: 86400
         });
         res.status(200).send({ auth: true, token: token });
-    })
 
+    } catch (error) {
+        console.log(error);
+
+    }
 });
 
-router.get('/me',  (req, res) => {
+router.get('/me', (req, res) => {
     var token = req.headers['x-access-token'];
     if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
 
@@ -41,6 +37,24 @@ router.get('/me',  (req, res) => {
         console.log(user);
         res.status(200).json(user);
     });
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        var user = await userDB.getUserById(req.body.email);
+        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+
+        var token = jwt.sign({ id: user._id }, config.secret, {
+            expiresIn: 86400 // expires in 24 hours
+        });
+
+        res.status(200).send({ auth: true, token: token });
+    } catch (error) {
+        console.log(error);
+    }
+
 });
 
 module.exports = router;
--- db/userDB.js
+++ db/userDB.js
@@ -3,12 +3,25 @@ const Pool = require('pg').Pool
 const pool = new Pool();
 
 const getUserById = async (id) => {
-    var res = await  pool.query('SELECT name,lastname,email FROM USERS WHERE email = $1',[id]);
+    var res = await  pool.query('SELECT * FROM USERS WHERE email = $1',[id]);
     console.log(res.rows[0]);
     return res.rows[0];
   };
 
+const createUser = async (name,lastname,email,password) => {
+
+    try {
+        var result = await pool.query('INSERT INTO users (name, lastname, email, password) VALUES ($1, $2, $3, $4)', [name, lastname, email, password]);
+        return result.id;
+    } catch (error) {
+       console.log(error); 
+    }
+    
+    
+}
+
 
   module.exports = {
-    getUserById
+    getUserById,
+    createUser
   };
