0b1f578267eadbd0e41ddee80054b00909e262e7
Santiago <santiagowhite99@hotmail.com>
2020-08-13 23:53:33 -0500

    error

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -4,12 +4,27 @@ var userDB = require('../db/userDB');
 var verifyToken = require('../auth/verifyToken');
 var router = express.Router();
 
-router.get('/eventos',verifyToken, async(req,res) => {
+router.get('/events',verifyToken, async(req,res) => {
     try {
         var loggedUser = await userDB.getUserById(req.userId);
         console.log(loggedUser);
         var events = await eventsDB.getUserEvents(loggedUser.email);
         res.json(events);
+    } catch (error) {
+        
+    }
+});
+
+
+router.post('/events',verifyToken,async(req,res)=> {
+    try {
+        const loggedUser = await userDB.getUserById(req.userId);
+        (event_name, event_category, event_place, event_address, event_initial_date,
+            event_final_date,event_type) = req.body;
+        
+        await eventsDB.createEvent(event_name,event_category,event_place,event_address,event_initial_date,event_final_date,
+            event_type,loggedUser.email);
+        res.redirect('/events');
     } catch (error) {
         
     }
